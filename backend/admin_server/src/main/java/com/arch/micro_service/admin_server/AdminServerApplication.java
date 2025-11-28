fe8c5769a68e053c9afb7f1c72d8f99077130e4d
SRIRAM9487 <sriram9487tk@gmail.com>
2025-11-28 13:26:20 +0530

    admin dash board implemented

--- backend/admin_server/src/main/java/com/arch/micro_service/admin_server/AdminServerApplication.java
+++ backend/admin_server/src/main/java/com/arch/micro_service/admin_server/AdminServerApplication.java
@@ -1,15 +1,18 @@
-package com.arch.micro_service.auth_server;
+package com.arch.micro_service.admin_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
 @SpringBootApplication
 @EnableDiscoveryClient
-public class AuthServerApplication {
+@EnableAdminServer
+public class AdminServerApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(AuthServerApplication.class, args);
+    SpringApplication.run(AdminServerApplication.class, args);
   }
 
 }
--- backend/notification_server/src/main/java/com/arch/micro_service/notification_server/NotificationServerApplication.java
+++ backend/notification_server/src/main/java/com/arch/micro_service/notification_server/NotificationServerApplication.java
@@ -2,8 +2,10 @@ package com.arch.micro_service.notification_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NotificationServerApplication {
 
   public static void main(String[] args) {
